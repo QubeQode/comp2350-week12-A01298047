e7113e5911e2405bd569453ccfc5ceaabac8803a
QubeQode <77304768+QubeQode@users.noreply.github.com>
2023-04-04 16:27:13 -0700

    Undo URI changes

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,7 +3,7 @@ const is_qoddi = process.env.IS_QODDI || false;
 const databaseName = "lab_example";
 
 const qoddiURI = 
-"mongodb+srv://QubeQode:S1USCOwsauuk0jlk@cluster0.tnk74fa.mongodb.net/lab_example/?retryWrites=true&w=majority";
+"mongodb+srv://QubeQode:S1USCOwsauuk0jlk@cluster0.tnk74fa.mongodb.net/lab_example?retryWrites=true&w=majority";
 
 const localURI = "mongodb://127.0.0.1/lab_example?authSource=admin&retryWrites=true&w=majority"
 
